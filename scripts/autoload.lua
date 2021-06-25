956e50bb164e8f44e6a829b53f3e275e286aa398
dyphire <61936050+dyphire@users.noreply.github.com>
2021-06-25 16:50:02 +0800

    同步上游
    Former-commit-id: 8aa503c618da122506c59d2c2177bab41e1eb26c

--- scripts/autoload.lua
+++ scripts/autoload.lua
@@ -1,3 +1,10 @@
+--[[
+SOURCE_https://github.com/mpv-player/mpv/blob/master/TOOLS/lua/autoload.lua
+COMMIT_20200214_e162bcb
+自动加载当前目录的所有文件到播放列表
+根据mpv的机制，切换目录会清空当前列表
+--]]
+
 -- This script automatically loads playlist entries before and after the
 -- the currently played file. It does so by scanning the directory a file is
 -- located in when starting playback. It sorts the directory entries
@@ -5,19 +12,6 @@
 -- the internal playlist. (It stops if it would add an already existing
 -- playlist entry at the same position - this makes it "stable".)
 -- Add at most 5000 * 2 files when starting a file (before + after).
-
---[[
-To configure this script use file autoload.conf in directory script-opts (the "script-opts"
-directory must be in the mpv configuration directory, typically ~/.config/mpv/).
-
-Example configuration would be:
-
-disabled=no
-images=no
-videos=yes
-audio=yes
-
---]]
 
 MAXENTRIES = 5000
 
@@ -51,11 +45,11 @@ EXTENSIONS_VIDEO = Set {
 }
 
 EXTENSIONS_AUDIO = Set {
-    'aac', 'alac', 'ape', 'flac', 'm4a', 'mp3', 'ogg', 'ogm', 'opus', 'wav', 'wma'
+    'aac', 'alac', 'ape', 'flac', 'm4a', 'mka', 'mp3', 'ogg', 'ogm', 'opus', 'wav', 'wma'
 }
 
 EXTENSIONS_IMAGES = Set {
-    'apng', 'avif', 'bmp', 'gif', 'heic', 'heif', 'jpeg', 'jpg', 'png', 'svg', 'tif', 'tiff', 'uci', 'webp'
+    'apng', 'avif', 'bmp', 'gif', 'heic', 'heif', 'jfif', 'jpeg', 'jpg', 'png', 'svg', 'tif', 'tiff', 'uci', 'webp'
 }
 
 EXTENSIONS = Set {}
