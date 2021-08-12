21af2240df7e6cf3a5d9d40ced0622947c698248
dyphire <qimoge@gmail.com>
2021-08-12 21:50:11 +0800

    同步上游
    Former-commit-id: 52e97ebc311f98a10ccc35c9f46a4e33ffcd6d36

--- shaders/KrigBilateral.glsl
+++ shaders/KrigBilateral.glsl
@@ -19,13 +19,13 @@
 //!SAVE LOWRES_Y
 //!WIDTH LUMA.w
 //!WHEN CHROMA.w LUMA.w <
-//!DESC KrigBilateral Downscaling Y 1
+//!DESC KrigBilateral Downscaling Y pass 1
 
 #define offset      vec2(0)
 
 #define axis        1
 
-#define Kernel(x)   dot(vec3(0.426590713671539, -0.496560619088564, 0.0768486672398968), cos(vec3(0, 1, 2) * acos(-1.) * (x + 1.)))
+#define Kernel(x)   dot(vec3(0.42659, -0.49656, 0.076849), cos(vec3(0, 1, 2) * acos(-1.) * (x + 1.)))
 
 vec4 hook() {
     // Calculate bounds
@@ -56,13 +56,13 @@ vec4 hook() {
 //!BIND LOWRES_Y
 //!SAVE LOWRES_Y
 //!WHEN CHROMA.w LUMA.w <
-//!DESC KrigBilateral Downscaling Y 2
+//!DESC KrigBilateral Downscaling Y pass 2
 
 #define offset      vec2(0)
 
 #define axis        0
 
-#define Kernel(x)   dot(vec3(0.426590713671539, -0.496560619088564, 0.0768486672398968), cos(vec3(0, 1, 2) * acos(-1.) * (x + 1.)))
+#define Kernel(x)   dot(vec3(0.42659, -0.49656, 0.076849), cos(vec3(0, 1, 2) * acos(-1.) * (x + 1.)))
 
 vec4 hook() {
     // Calculate bounds
