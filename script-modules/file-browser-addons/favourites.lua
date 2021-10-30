ed95154353d601c39368172bfa6b54517e128edc
dyphire <qimoge@gmail.com>
2021-10-30 11:41:43 +0800

    同步上游
    Former-commit-id: 56536d58180973a932cc522582d84736a467ea7f

--- script-modules/file-browser-addons/favourites.lua
+++ script-modules/file-browser-addons/favourites.lua
@@ -1,39 +1,19 @@
 --[[
     An addon for mpv-file-browser which adds a Favourites path that can be loaded from the ROOT
-
-
-    Designed to work with the following custom keybinds:
-    {
-        "key": "F",
-        "command": ["script-message", "favourites/add_favourite", "%f"]
-    },
-    {
-        "key": "F",
-        "command": ["script-message", "favourites/remove_favourite", "%f"],
-        "parser": "favourites"
-    },
-    {
-        "key": "Ctrl+UP",
-        "command": [
-            ["script-binding", "file_browser/dynamic/scroll_up"],
-            ["script-message", "favourites/move_up", "%f"]
-        ],
-        "parser": "favourites"
-    },
-    {
-        "key": "Ctrl+DOWN",
-        "command": [
-            ["script-binding", "file_browser/dynamic/scroll_down"],
-            ["script-message", "favourites/move_down", "%f"]
-        ],
-        "parser": "favourites"
-    }
 ]]--
 
 local mp = require "mp"
 local msg = require "mp.msg"
 local utils = require "mp.utils"
-local save_path = mp.command_native({"expand-path", "~~/script-opts/file_browser_favourites"})
+local save_path = mp.command_native({"expand-path", "~~/script-opts/file_browser_favourites.txt"})
+do
+    local file = io.open(save_path, "a+")
+    if not file then
+        msg.error("cannot access file", ("%q"):format(save_path), "make sure that the directory exists")
+        return {}
+    end
+    file:close()
+end
 
 local favourites = nil
 local favs = {
@@ -79,7 +59,7 @@ function favs:can_parse(directory)
     return directory:find("Favourites/") == 1
 end
 
-function favs:parse(directory)
+function favs:parse(directory, ...)
     if not favourites then update_favourites() end
     if directory == "Favourites/" then
         local opts = {
@@ -96,7 +76,7 @@ function favs:parse(directory)
 
         local _, finish = directory:find("Favourites/([^/]+/?)")
         local full_path = (full_paths[name] or "")..directory:sub(finish+1)
-        local list, opts = self:defer(full_path or "")
+        local list, opts = self:defer(full_path or "", ...)
 
         if not list then return nil end
         opts.index = self:get_index()
@@ -114,7 +94,7 @@ function favs:parse(directory)
 
     local path = full_paths[ directory:match("([^/]+/?)$") or "" ]
 
-    local list, opts = self:defer(path)
+    local list, opts = self:defer(path, ...)
     if not list then return nil end
     opts.directory = opts.directory or path
     return list, opts
@@ -126,16 +106,27 @@ local function get_favourite(path)
     end
 end
 
+--update the browser with new contents of the file
+local function update_browser()
+    if favs.get_directory():find("[fF]avourites/") then
+        if favs.get_directory():find("[fF]avourites/$") then
+            local cursor = favs.get_selected_index()
+            favs.rescan_directory()
+            favs.set_selected_index(cursor)
+            favs.update_ass()
+        else
+            favs.clear_cache()
+        end
+    end
+end
+
+--write the contents of favourites to the file
 local function write_to_file()
     local file = io.open(save_path, "w+")
     for _, item in ipairs(favourites) do
         file:write(string.format("%s\n", item.path))
     end
     file:close()
-    if favs.get_directory() == "Favourites/" then
-        favs.cursor = favs.get_selected_index()
-        mp.commandv("script-binding", "file_browser/dynamic/reload")
-    end
 end
 
 local function add_favourite(path)
@@ -163,9 +154,39 @@ local function move_favourite(path, direction)
     write_to_file()
 end
 
+local function toggle_favourite(cmd, state, co)
+    local path = favs.get_full_path(state.list[state.selected], state.directory)
+
+    if state.directory:find("[fF]avourites/$") then remove_favourite(path)
+    else add_favourite(path) end
+    update_browser()
+end
+
+local function move_key(cmd, state, co)
+    if not state.directory:find("[fF]avourites/") then return false end
+    local path = favs.get_full_path(state.list[state.selected], state.directory)
+
+    local cursor = favs.get_selected_index()
+    if cmd.name == favs:get_id().."/move_up" then
+        move_favourite(path, -1)
+        favs.set_selected_index(cursor-1)
+    else
+        move_favourite(path, 1)
+        favs.set_selected_index(cursor+1)
+    end
+    update_browser()
+end
+
+update_favourites()
 mp.register_script_message("favourites/add_favourite", add_favourite)
 mp.register_script_message("favourites/remove_favourite", remove_favourite)
 mp.register_script_message("favourites/move_up", function(path) move_favourite(path, -1) end)
 mp.register_script_message("favourites/move_down", function(path) move_favourite(path, 1) end)
+
+favs.keybinds = {
+    { "F", "toggle_favourite", toggle_favourite, {}, },
+    { "Ctrl+UP", "move_up", move_key, {repeatable = true} },
+    { "Ctrl+DOWN", "move_down", move_key, {repeatable = true} },
+}
 
 return favs
