c2987aa8728951d8052a965d2533b6d4692719a6
dyphire <qimoge@gmail.com>
2021-12-07 08:34:55 +0800

    同步上游脚本更新
    修正osc脚本，优化字幕轨道选择
    
    update manager.json
    
    
    Former-commit-id: 6f55cc0becba2f084783f0512db48e7e648a3ad1 [formerly eea97bebe1528ed6988b8ee45dc27ceeae83037f]
    Former-commit-id: df5d235060dd49cf2d74800348b8ed87b2e9aba2
    Former-commit-id: c3663e4e28bcd5b11192a18782b23ba0e3b256c5

--- scripts/cycle_adevice.lua
+++ scripts/cycle_adevice.lua
@@ -1,7 +1,6 @@
 --[[
 SOURCE_ https://gist.github.com/bitingsock/ad58ee5da560ecb922fa4a867ac0ecfd
-COMMIT_20200802
-
+COMMIT_ 20200802
 自定义快捷键 快速切换音频输出设备
 此脚本优于在 input.conf 中使用 cycle-values audio-device 参数的方案
 示例在 input.conf 中写入两行：
@@ -12,12 +11,11 @@ ALT+a  script-binding  cycle_adevice/next
 local opt = require 'mp.options'
 
 local user_opts = {
-    api = "wasapi"   -- <wasapi/openal/sdl> 限制音频接口
+    api = ""   -- <wasapi/openal/sdl> 限制音频接口
 }
 
 opt.read_options(user_opts)
 
-local api = user_opts.api
 local deviceList = {}
 local function cycle_adevice(s, e, d)
 	while s ~= e + d do -- until the loop would cycle back to the number we started on
@@ -29,12 +27,12 @@ local function cycle_adevice(s, e, d)
 					s = 0 --then start from the beginning
 				end
 				s = s + d --next device
-				if string.find(deviceList[s].name, api, 1, true) then
+				if string.find(deviceList[s].name, user_opts.api, 1, true) then
 					mp.set_property("audio-device",deviceList[s].name)
 					deviceList[s].description = "■ "..deviceList[s].description
 					local list = "音频输出设备：\n"
 					for i=1,#deviceList do
-						if string.find(deviceList[i].name, api, 1, true) then
+						if string.find(deviceList[i].name, user_opts.api, 1, true) then
 							if deviceList[i].name~=deviceList[s].name then list = list.."□ " end
 							list=list..deviceList[i].description.."\n"
 						end
@@ -56,10 +54,10 @@ local function cycle_back()
 	deviceList = mp.get_property_native("audio-device-list")
 	cycle_adevice(#deviceList, 1, -1) --'s'tart at last device, 'e'nd at device 1, iterate backward 'd'elta=-1
 end
-local function cycle_forward()
+local function cycle_next()
 	deviceList = mp.get_property_native("audio-device-list")
 	cycle_adevice(1, #deviceList, 1) --'s'tart at device 1, 'e'nd at last device, iterate forward 'd'elta=1
 end
 
-mp.add_key_binding(nil, "next", cycle_forward)
+mp.add_key_binding(nil, "next", cycle_next)
 mp.add_key_binding(nil, "back", cycle_back)
