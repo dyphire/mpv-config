71ae17937373226206851df3c4adc9ebc2fb9922
dyphire <qimoge@gmail.com>
2022-02-08 23:18:42 +0800

    SSimSuperRes.glsl 同步上游更新
    Former-commit-id: 855a3c6d5b3882c2befbf5531baea167b1918acd [formerly 9783419458f031db8a751e3a3a0ad0baf9b872fb]
    Former-commit-id: ea87a2efae42f04284d6ec34c6edafffc7827747
    Former-commit-id: 0d93f29bc14c38bc561e8f80ceb12679d93d48ff

--- shaders/igv/SSimSuperRes.glsl
+++ shaders/igv/SSimSuperRes.glsl
@@ -29,10 +29,9 @@
 #define Kernel(x)   MN(0.334, 0.333, abs(x))
 #define taps        2.0
 
-#define Luma(rgb)   ( dot(rgb*rgb, vec3(0.2126, 0.7152, 0.0722)) )
+#define Luma(rgb)   dot(rgb*rgb, vec3(0.2126, 0.7152, 0.0722))
 
 vec4 hook() {
-    // Calculate bounds
     float low  = ceil((HOOKED_pos - taps/input_size) * HOOKED_size - offset - 0.5)[axis];
     float high = floor((HOOKED_pos + taps/input_size) * HOOKED_size - offset - 0.5)[axis];
 
@@ -73,10 +72,9 @@ vec4 hook() {
 #define Kernel(x)   MN(0.334, 0.333, abs(x))
 #define taps        2.0
 
-#define Luma(rgb)   ( dot(rgb*rgb, vec3(0.2126, 0.7152, 0.0722)) )
+#define Luma(rgb)   dot(rgb*rgb, vec3(0.2126, 0.7152, 0.0722))
 
 vec4 hook() {
-    // Calculate bounds
     float low  = ceil((LOWRES_pos - taps/input_size) * LOWRES_size - offset - 0.5)[axis];
     float high = floor((LOWRES_pos + taps/input_size) * LOWRES_size - offset - 0.5)[axis];
 
@@ -110,35 +108,36 @@ vec4 hook() {
 //!COMPONENTS 2
 //!DESC SSSR var
 
-#define spread      1.0 / 1000.0
+#define spread      1.0 / 4.0
 
 #define GetL(x,y)   PREKERNEL_tex(PREKERNEL_pt * (PREKERNEL_pos * input_size + tex_offset + vec2(x,y))).rgb
 #define GetH(x,y)   LOWRES_texOff(vec2(x,y)).rgb
 
-#define Luma(rgb)   ( dot(rgb*rgb, vec3(0.2126, 0.7152, 0.0722)) )
+#define Luma(rgb)   dot(rgb*rgb, vec3(0.2126, 0.7152, 0.0722))
+#define diff(x,y)   vec2(Luma((GetL(x,y) - meanL)), Luma((GetH(x,y) - meanH)))
 
 vec4 hook() {
-    vec3 meanL = vec3(0);
-    vec3 meanH = vec3(0);
-    for (int X=-1; X<=1; X++)
-    for (int Y=-1; Y<=1; Y++) {
-        meanL += GetL(X,Y) * pow(spread, abs(float(X)) + abs(float(Y)));
-        meanH += GetH(X,Y) * pow(spread, abs(float(X)) + abs(float(Y)));
+    vec3 meanL = GetL(0,0);
+    vec3 meanH = GetH(0,0);
+    for (int X=-1; X<=1; X+=2) {
+        meanL += GetL(X,0) * spread;
+        meanH += GetH(X,0) * spread;
     }
-    meanL /= (1.0 + 4.0*spread + 4.0*spread*spread);
-    meanH /= (1.0 + 4.0*spread + 4.0*spread*spread);
-
-    float varL = 0.0;
-    float varH = 0.0;
-    for (int X=-1; X<=1; X++)
-    for (int Y=-1; Y<=1; Y++) {
-        varL += Luma(abs(GetL(X,Y) - meanL)) * pow(spread, abs(float(X)) + abs(float(Y)));
-        varH += Luma(abs(GetH(X,Y) - meanH)) * pow(spread, abs(float(X)) + abs(float(Y)));
+    for (int Y=-1; Y<=1; Y+=2) {
+        meanL += GetL(0,Y) * spread;
+        meanH += GetH(0,Y) * spread;
     }
-    varL = max(varL / (spread + 4.0*spread + 4.0*spread*spread), 1e-6);
-    varH = max(varH / (spread + 4.0*spread + 4.0*spread*spread), 1e-6);
+    meanL /= (1.0 + 4.0*spread);
+    meanH /= (1.0 + 4.0*spread);
 
-    return vec4(varL, varH, 0, 0);
+    vec2 var = diff(0,0);
+    for (int X=-1; X<=1; X+=2)
+        var += diff(X,0) * spread;
+
+    for (int Y=-1; Y<=1; Y+=2)
+        var += diff(0,Y) * spread;
+
+    return vec4(max(var / (1.0 + 4.0*spread), vec2(1e-6)), 0, 0);
 }
 
 //!HOOK POSTKERNEL
@@ -149,25 +148,26 @@ vec4 hook() {
 //!WHEN NATIVE_CROPPED.h OUTPUT.h <
 //!DESC SSSR final pass
 
+#define oversharp   0.0
+
 // -- Window Size --
 #define taps        3.0
 #define even        (taps - 2.0 * floor(taps / 2.0) == 0.0)
 #define minX        int(1.0-ceil(taps/2.0))
 #define maxX        int(floor(taps/2.0))
 
-#define Kernel(x)   ( cos(acos(-1.0)*(x)/taps) ) // Hann kernel
+#define Kernel(x)   cos(acos(-1.0)*(x)/taps) // Hann kernel
 
 // -- Input processing --
-#define var(x,y)    ( var_tex(var_pt * (pos + vec2(x,y) + 0.5)).rg )
-#define GetL(x,y)   ( PREKERNEL_tex(PREKERNEL_pt * (pos + tex_offset + vec2(x,y) + 0.5)).rgb )
-#define GetH(x,y)   ( LOWRES_tex(LOWRES_pt * (pos + vec2(x,y) + 0.5)) )
+#define var(x,y)    var_tex(var_pt * (pos + vec2(x,y) + 0.5)).rg
+#define GetL(x,y)   PREKERNEL_tex(PREKERNEL_pt * (pos + tex_offset + vec2(x,y) + 0.5)).rgb
+#define GetH(x,y)   LOWRES_tex(LOWRES_pt * (pos + vec2(x,y) + 0.5))
 
-#define Luma(rgb)   ( dot(rgb*rgb, vec3(0.2126, 0.7152, 0.0722)) )
+#define Luma(rgb)   dot(rgb*rgb, vec3(0.2126, 0.7152, 0.0722))
 
 vec4 hook() {
     vec4 c0 = HOOKED_texOff(0);
 
-    // Calculate position
     vec2 pos = HOOKED_pos * LOWRES_size - vec2(0.5);
     vec2 offset = pos - (even ? floor(pos) : round(pos));
     pos -= offset;
@@ -175,7 +175,7 @@ vec4 hook() {
     vec2 mVar = vec2(0.0);
     for (int X=-1; X<=1; X++)
     for (int Y=-1; Y<=1; Y++) {
-        vec2 w = clamp(1.5 - abs(vec2(X,Y) - offset), 0., 1.);
+        vec2 w = clamp(1.5 - abs(vec2(X,Y) - offset), 0.0, 1.0);
         mVar += w.r * w.g * vec2(GetH(X,Y).a, 1.0);
     }
     mVar.r /= mVar.g;
@@ -187,10 +187,10 @@ vec4 hook() {
     for (int X = minX; X <= maxX; X++)
     for (int Y = minX; Y <= maxX; Y++)
     {
-        float R = -sqrt(var(X,Y).r / (var(X,Y).g + mVar.r));
+        float R = (-1.0 - oversharp) * sqrt(var(X,Y).r / (var(X,Y).g + mVar.r));
 
         vec2 krnl = Kernel(vec2(X,Y) - offset);
-        float weight = krnl.r * krnl.g / (Luma(abs(c0.rgb - GetH(X,Y).rgb)) + GetH(X,Y).a);
+        float weight = krnl.r * krnl.g / (Luma((c0.rgb - GetH(X,Y).rgb)) + GetH(X,Y).a);
 
         diff += weight * (GetL(X,Y) + GetH(X,Y).rgb * R + (-1.0 - R) * (c0.rgb));
         weightSum += weight;
