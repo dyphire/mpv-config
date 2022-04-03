c39a1ad57257b98f9be14e3b01c5169be0e12ca1
dyphire <qimoge@gmail.com>
2022-04-03 11:38:11 +0800

    OSC同步上游更新 上游代码：https://github.com/hooke007/MPV_lazy/commit/30b5728acec7384fd6d188a2b47f6a129232ca55

--- scripts/thumbnailer_osc.lua
+++ scripts/thumbnailer_osc.lua
@@ -3,8 +3,8 @@ SOURCE_ https://github.com/mpv-player/mpv/blob/master/player/lua/osc.lua
 COMMIT_ 20220206 0197729
 SOURCE_ https://github.com/deus0ww/mpv-conf/blob/master/scripts/Thumbnailer_OSC.lua
 COMMIT_ 20220207 8b57a11
-SOURCE_ https://github.com/hooke007/MPV_lazy/commit/9116558a85fc8468d30dfe785c83a67838f0f6da
-COMMIT_ 20220325 9116558
+SOURCE_ https://github.com/hooke007/MPV_lazy/blob/main/portable_config/scripts/osc_lazy.lua
+COMMIT_ 20220331 30b5728
 改进版本的OSC，须禁用原始mpv的内置OSC，且不兼容其它OSC类脚本，实现全部功能需搭配额外两个缩略图引擎脚本（Thumbnailer）。
 示例在 input.conf 中写入：
 SHIFT+DEL  script-binding thumbnailer_osc/visibility  # 切换thumbnailer_osc的可见性
@@ -63,14 +63,14 @@ local user_opts = {
     seekrangestyle = "inverted",        -- bar, line, slider, inverted or none
     seekrangeseparate = true,           -- wether the seekranges overlay on the bar-style seekbar
     seekrangealpha = 200,               -- transparency of seekranges
-    seekbarkeyframes = true,            -- use keyframes when dragging the seekbar
+    seekbarkeyframes = true,            -- use keyframes when dragging the seekbar       -- 现不受全局hr-seek的控制 
     title = "${media-title}",           -- string compatible with property-expansion
                                         -- to be shown as OSC title
     tooltipborder = 1,                  -- border of tooltip in bottom/topbar
-    timetotal = true,                   -- display total time instead of remaining time?   -- 原版为false
+    timetotal = true,                   -- display total time instead of remaining time? -- 原版为false
     timems = false,                     -- display timecodes with milliseconds?
     visibility = "auto",                -- only used at init to set visibility_mode(...)
-    boxmaxchars = 150,                  -- title crop threshold for box layout             -- 原版为80
+    boxmaxchars = 150,                  -- title crop threshold for box layout           -- 原版为80
     boxvideo = false,                   -- apply osc_param.video_margins to video
     windowcontrols = "auto",            -- whether to show window controls
     windowcontrols_alignment = "right", -- which side to show window controls on
@@ -85,8 +85,8 @@ local user_opts = {
     wctitle = "${media-title}",         -- 无边框的上方标题
     sub_title = " ",                    -- bottombox布局的右侧子标题
     sub_title2 = "对比[${contrast}]  亮度[${brightness}]  伽马[${gamma}]  饱和[${saturation}]  色相[${hue}]", -- bottombox布局的临时右侧子标题
-    seekbar_scrollseek = "fast",        -- 进度条的滚轮跳转模式 "fast" "second" "frame"
-    showonpause = false,                -- 在暂停时显示 OSC
+    seekbar_scrollseek = "fast",        -- 进度条的滚轮跳转模式 "fast" "second" "frame" 。不受全局hr-seek的控制
+    showonpause = false,                -- 在暂停时常驻 OSC
     showonstart = false,                -- 在播放开始或当播放下一个文件时显示 OSC
     showonseek = false,                 -- 在跳转时显示 OSC
     font = "sans",                      -- OSC的全局字体显示
@@ -1956,8 +1956,10 @@ layouts["bottombox"] = function ()
     lo.button.maxchars = user_opts.boxmaxchars
 
     if (user_opts.layout == "bottombox") then
-        if (osc_param.display_aspect < 1) then
-            lo.button.maxchars = 50
+        if (osc_param.display_aspect < 1) then lo.button.maxchars = 50
+            if (osc_param.display_aspect < 0.8) then lo.button.maxchars = 35
+                if (osc_param.display_aspect < 0.6) then lo.button.maxchars = 20 end
+            end
         end
     end
 
@@ -2543,8 +2545,12 @@ function osc_init()
 
     -- playlist buttons
 
-    -- prev
+    -- prev -- 上一个
     ne = new_element("pl_prev", "button")
+
+    if (user_opts.layout == "bottombox") then
+        ne.visible = (osc_param.display_aspect > 0.5)
+    end
 
     ne.content = "\238\132\144"
     ne.enabled = (pl_pos > 1) or (loop ~= "no")
@@ -2560,8 +2566,12 @@ function osc_init()
     ne.eventresponder["mbtn_right_up"] =
         function () show_message(get_playlist(), 3) end
 
-    --next
+    --next -- 下一个
     ne = new_element("pl_next", "button")
+
+    if (user_opts.layout == "bottombox") then
+        ne.visible = (osc_param.display_aspect > 0.5)
+    end
 
     ne.content = "\238\132\129"
     ne.enabled = (have_pl and (pl_pos < pl_count)) or (loop ~= "no")
@@ -2803,9 +2813,9 @@ function osc_init()
             local seekto = get_slider_value(element)
             if (element.state.lastseek == nil) or
                 (not (element.state.lastseek == seekto)) then
-                    local flags = "absolute-percent"
+                    local flags = "absolute-percent+keyframes" -- 防止--hr-seek的影响
                     if not user_opts.seekbarkeyframes then
-                        flags = flags .. "+exact"
+                        flags = "absolute-percent+exact"       -- 防止--hr-seek的影响
                     end
                     mp.commandv("seek", seekto, flags)
                     element.state.lastseek = seekto
@@ -2821,21 +2831,25 @@ function osc_init()
     ne.eventresponder["mbtn_right_dbl_press"] = function (element) mp.commandv('script-message', 'Thumbnailer-double') end
 
     ne.eventresponder["wheel_up_press"] = function ()
-        if user_opts.seekbar_scrollseek == "fast" then mp.commandv('seek', -1, 'keyframes')
-        elseif user_opts.seekbar_scrollseek == "second" then mp.commandv('seek', -1, 'exact')
+        if user_opts.seekbar_scrollseek == "fast" then mp.commandv('seek', -0.1, 'keyframes')
+        elseif user_opts.seekbar_scrollseek == "second" then mp.commandv('seek', -0.1, 'exact')
         elseif user_opts.seekbar_scrollseek == "frame" then mp.commandv('frame-back-step')
         end
     end
 
     ne.eventresponder["wheel_down_press"] = function ()
-        if user_opts.seekbar_scrollseek == "fast" then mp.commandv('seek', 1, 'keyframes')
-        elseif user_opts.seekbar_scrollseek == "second" then mp.commandv('seek', 1, 'exact')
+        if user_opts.seekbar_scrollseek == "fast" then mp.commandv('seek', 0.1, 'keyframes')
+        elseif user_opts.seekbar_scrollseek == "second" then mp.commandv('seek', 0.1, 'exact')
         elseif user_opts.seekbar_scrollseek == "frame" then mp.commandv('frame-step')
         end
     end
 
-    -- tc_left (current pos)
+    -- tc_left (current pos) -- 当前时间
     ne = new_element("tc_left", "button")
+
+    if (user_opts.layout == "bottombox") then
+        ne.visible = (osc_param.display_aspect > 0.5)
+    end
 
     ne.content = function ()
         if (state.tc_ms) then
@@ -2849,10 +2863,12 @@ function osc_init()
         request_init()
     end
 
-    -- tc_right (total/remaining time)
+    -- tc_right (total/remaining time) -- 总/剩余时间
     ne = new_element("tc_right", "button")
 
     ne.visible = (mp.get_property_number("duration", 0) > 0)
+    if (user_opts.layout == "bottombox") and (mp.get_property_number("duration", 0) > 0) then ne.visible = (osc_param.display_aspect > 0.5) end
+
     ne.content = function ()
         if (state.rightTC_trem) then
             if state.tc_ms then
@@ -3654,7 +3670,7 @@ function visibility_mode(mode, no_osd)
     utils.shared_script_property_set("osc-visibility", mode)
 
     if not no_osd and tonumber(mp.get_property("osd-level")) >= 1 then
-        mp.osd_message("Thumbnailer_OSC的可见性：" .. mode)
+        mp.osd_message("OSC的可见性：" .. mode)
     end
 
     -- Reset the input state on a mode change. The input state will be
