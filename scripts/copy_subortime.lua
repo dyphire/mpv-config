58b347191c84a1abd595bcc6c5db686de5d630f0
dyphire <qimoge@gmail.com>
2022-05-26 21:52:42 +0800

    将一些可能的内容设置为本地，以免污染全局范围

--- scripts/copy_subortime.lua
+++ scripts/copy_subortime.lua
@@ -9,7 +9,7 @@
 
 require "mp"
 
-function set_clipboard (text)
+local function set_clipboard (text)
   local echo
   if text ~= "" then
     for i = 1, 2 do text = text:gsub("[%^&\\<>|]", "^%0") end
@@ -20,7 +20,7 @@ function set_clipboard (text)
   mp.commandv("run", "cmd.exe", "/d", "/c", echo .. " | clip")
 end
 
-function copy_time()
+local function copy_time()
   local time_pos = mp.get_property_number("time-pos")
   local time_in_seconds = time_pos
   local time_seg = time_pos % 60
@@ -34,7 +34,7 @@ function copy_time()
   mp.osd_message(string.format("已复制当前时间: %s", time))
 end
 
-function copy_subtitle ()
+local function copy_subtitle ()
   local subtitle = mp.get_property("sub-text")
   set_clipboard(subtitle)
   mp.osd_message("已复制当前字幕内容")
