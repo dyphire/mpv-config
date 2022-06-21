f7dcba3d17cfeb94346ba7622bfb80b70024d3a8
dyphire <qimoge@gmail.com>
2022-06-21 23:02:33 +0800

    小更新
    Former-commit-id: f7b4fd1d167419f8f330a06c025c2c8378d42c83

--- scripts/chapter_make_read.lua
+++ scripts/chapter_make_read.lua
@@ -1,5 +1,5 @@
 --[[
-  * chapter_make_read.lua v.2022-06-20
+  * chapter_make_read.lua v.2022-06-21
   *
   * AUTHORS: dyphire
   * License: MIT
@@ -7,23 +7,16 @@
 --]]
 
 -- Implementation read and automatically load the namesake external chapter file.
--- The external chapter files should conform to the following specifications.
+-- The external chapter files should conform to the following formats.
 -- Note: The time should strictly follow the 12-bit format of 'hh:mm:ss.sss' !!
 -- Note: The file encoding should be UTF-8 and the linebreak should be Unix(LF).
+-- Note: The script also supports reading OGM format and MediaInfo format in addition to the following formats.
 --[[
 00:00:00.000 A part
 00:00:40.312 OP
 00:02:00.873 B part
 00:10:44.269 C part
 00:22:40.146 ED
---]]
--- OR(not recommended)
---[[
-0:00:00 A part
-0:00:40 OP
-0:02:00 B part
-0:10:44 C part
-0:22:40 ED
 --]]
 
 -- This script also supports marks and creates external chapter files, usage:
@@ -63,26 +56,40 @@ end
 local function read_chapter_table()
 	local line_pos = 0
 	return read_chapter(function(line)
-        local h, m, s, t, c, l
-        if string.len(line) >= 7 and line:match('^%d+:%d+:%d+') ~= nil then
-            h = line:match('^%d%d?:')
+        local tt, h, m, s, t, n, l
+        if line:match("^%d+:%d+:%d+") ~= nil then
+            h = line:match("^%d%d?:")
             h = h:gsub(":", "")
-            m = line:match(':%d+:')
+            m = line:match(":%d+:")
             m = m:gsub(":", "")
-            s = line:gsub('^%d+:%d+:', "")
-            s = s:match('^%d+.%d*[,%s]?')
+            s = line:gsub("^%d+:%d+:", "")
+            s = s:match("^%d+.%d*[,%s]?")
             s = s:gsub("[,%s]$", "")
             t = h*3600 + m*60 + s
-            if string.len(line) >= 9 and line:match('[,%s].*') ~= nil then
-                c = line:match('[,%s].*')
-                c = c:gsub("^%s*(.-)%s*$", "%1")
-                c = c:gsub("^,", "")
-                c = c:gsub(": %a%a:", "")
+            if line:match("^%d+:%d+:%d+.%d*[,%s].*") ~= nil then
+                n = line:match("[,%s].*")
+                n = n:gsub("^,", "")
+                n = n:gsub("%s:%s%a?%a?:", "")
+                n = n:gsub("^%s*(.-)%s*$", "%1")
             end
             l = line
             line_pos = line_pos + 1
+        elseif line:match("^CHAPTER%d+=%d+:%d+:%d+") ~= nil then
+            tt = line:gsub("^CHAPTER%d+=", "")
+            h = tt:match("^%d%d?:")
+            h = h:gsub(":", "")
+            m = tt:match(":%d+:")
+            m = m:gsub(":", "")
+            s = tt:gsub("^%d+:%d+:", "")
+            t = h*3600 + m*60 + s
+            l = line
+            line_pos = line_pos + 1
+        elseif line:match("^CHAPTER%d+NAME=.*") ~= nil then
+            n = line:gsub("^CHAPTER%d+NAME=", "")
+            l = line
+            line_pos = line_pos + 1
         else return end
-        return {found_title = c, found_time = t, found_line = l}
+        return {found_title = n, found_time = t, found_line = l}
 	end)
 end
 
@@ -92,7 +99,7 @@ local function mark_chapter()
     local chapter_index = 0
     local chapters_time = {}
     local chapters_title = {}
-    local path = mp.get_property('path')
+    local path = mp.get_property("path")
     local dir, filename = utils.split_path(path)
     local fpath = dir:gsub("\\", "/")
     local fname = mp.get_property("filename/no-ext")
@@ -105,8 +112,9 @@ local function mark_chapter()
 
     if not list_contents then return end
     for i = 1, #list_contents do
-        if tonumber(list_contents[i].found_time) >= 0 then
-            table.insert(chapters_time, tonumber(list_contents[i].found_time))
+        local chapter_time = tonumber(list_contents[i].found_time)
+        if chapter_time ~= nil and chapter_time >= 0 then
+            table.insert(chapters_time, chapter_time)
         end
         if list_contents[i].found_title ~= nil then
             table.insert(chapters_title, list_contents[i].found_title)
@@ -119,7 +127,7 @@ local function mark_chapter()
     for i = 1, #chapters_time do
         chapter_index = chapter_index + 1
         all_chapters[chapter_index] = {
-            title = chapters_title[i] or ('Chapter ' .. chapter_index),
+            title = chapters_title[i] or ("Chapter " .. string.format("%02.f", chapter_index)),
             time = chapters_time[i]
         }
     end
@@ -127,7 +135,7 @@ local function mark_chapter()
     table.sort(all_chapters, function(a, b) return a['time'] < b['time'] end)
 
     mp.set_property_native("chapter-list", all_chapters)
-    msg.info('load external chapter flie successful: ' .. chapter_fliename)
+    msg.info("load external chapter flie successful: " .. chapter_fliename)
 end
 
 local function create_chapter()
@@ -140,7 +148,7 @@ local function create_chapter()
 
     if chapter_count == 0 then
         all_chapters[1] = {
-            title = "chapter_1",
+            title = "Chapter 01",
             time = time_pos
         }
         -- We just set it to zero here so when we add 1 later it ends up as 1
@@ -158,7 +166,7 @@ local function create_chapter()
             all_chapters[i + 1] = all_chapters[i]
         end
         all_chapters[curr_chapter+2] = {
-            title = "chapter_" .. curr_chapter+2,
+            title = "Chapter " .. string.format("%02.f", curr_chapter+2),
             time = time_pos
         }
     end
@@ -256,7 +264,7 @@ local function write_chapter_xml()
     mp.osd_message("Export file to: "..out_path, 3)
 end
 
-mp.register_event("file-loaded", mark_chapter)
+mp.add_hook("on_preloaded", 50, mark_chapter)
 
 mp.register_script_message("create_chapter", create_chapter, {repeatable=true})
 mp.register_script_message("write_chapter", write_chapter, {repeatable=false})
--- scripts/file-browser.lua
+++ scripts/file-browser.lua
@@ -29,6 +29,13 @@ local o = {
     --only show files compatible with mpv
     filter_files = true,
 
+    --experimental feature that recurses directories concurrently when
+    --appending items to the playlist
+    concurrent_recursion = false,
+
+    --maximum number of recursions that can run concurrently
+    max_concurrency = 100,
+
     --enable custom keybinds
     custom_keybinds = false,
 
@@ -331,6 +338,12 @@ function API.coroutine.callback()
     end
 end
 
+--puts the current coroutine to sleep for the given number of seconds
+function API.coroutine.sleep(n)
+    mp.add_timeout(n, API.coroutine.callback())
+    coroutine.yield()
+end
+
 --runs the given function in a coroutine, passing through any additional arguments
 --this is for triggering an event in a coroutine
 function API.coroutine.run(fn, ...)
@@ -434,6 +447,11 @@ function API.valid_file(file)
     return true
 end
 
+--returns whether or not the item can be parsed
+function API.parseable_item(item)
+    return item.type == "dir" or parseable_extensions[API.get_extension(item.name, "")]
+end
+
 --removes items and folders from the list
 --this is for addons which can't filter things during their normal processing
 function API.filter(t)
@@ -533,7 +551,11 @@ local function copy_table_recursive(t, references)
     if type(t) ~= "table" then return t end
     if references[t] then return references[t] end
 
-    local copy = setmetatable( {}, { __original = t } )
+    local mt = {
+        __original = t,
+        __index = getmetatable(t)
+    }
+    local copy = setmetatable({}, mt)
     references[t] = copy
 
     for key, value in pairs(t) do
@@ -639,7 +661,7 @@ end
 --detects whether or not to highlight the given entry as being played
 local function highlight_entry(v)
     if current_file.name == nil then return false end
-    if v.type == "dir" or parseable_extensions[API.get_extension(v.name, "")] then
+    if API.parseable_item(v) then
         return current_file.directory:find(API.get_full_path(v), 1, true)
     else
         return current_file.path == API.get_full_path(v)
@@ -851,7 +873,7 @@ end
 local function select_prev_directory()
     if state.prev_directory:find(state.directory, 1, true) == 1 then
         local i = 1
-        while (state.list[i] and (state.list[i].type == "dir" or parseable_extensions[API.get_extension(state.list[i].name, "")])) do
+        while (state.list[i] and API.parseable_item(state.list[i])) do
             if state.prev_directory:find(API.get_full_path(state.list[i]), 1, true) then
                 state.selected = i
                 return
@@ -1064,7 +1086,7 @@ end
 --moves down a directory
 local function down_dir()
     local current = state.list[state.selected]
-    if not current or current.type ~= 'dir' and not parseable_extensions[API.get_extension(current.name, "")] then return end
+    if not current or not API.parseable_item(current) then return end
 
     cache:push()
     local directory, redirected = API.get_new_directory(current, state.directory)
@@ -1153,9 +1175,93 @@ end
 ------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------
 
+--adds a file to the playlist and changes the flag to `append-play` in preparation
+--for future items
+local function loadfile(file, opts)
+    if opts.flag == "replace" then msg.verbose("Playling file", file)
+    else msg.verbose("Appending", file, "to the playlist") end
+
+    if not mp.commandv("loadfile", file, opts.flag) then msg.warn(file) end
+    opts.flag = "append-play"
+    opts.items_appended = opts.items_appended + 1
+end
+
+--this function recursively loads directories concurrently in separate coroutines
+--results are saved in a tree of tables that allows asynchronous access
+local function concurrent_loadlist_parse(directory, load_opts, prev_dirs, item_t)
+    --prevents infinite recursion from the item.path or opts.directory fields
+    if prev_dirs[directory] then return end
+    prev_dirs[directory] = true
+
+    local list, list_opts = parse_directory(directory, { source = "loadlist" })
+    if list == root then return end
+
+    --if we can't parse the directory then append it and hope mpv fares better
+    if list == nil then
+        msg.warn("Could not parse", directory, "appending to playlist anyway")
+        item_t.type = "file"
+        return
+    end
+
+    directory = list_opts.directory or directory
+    if directory == "" then return end
+
+    --we must declare these before we start loading sublists otherwise the append thread will
+    --need to wait until the whole list is loaded (when synchronous IO is used)
+    item_t._sublist = list or {}
+    list._directory = directory
+
+    --launches new parse operations for directories, each in a different coroutine
+    for _, item in ipairs(list) do
+        if API.parseable_item(item) then
+            API.coroutine.run(concurrent_loadlist_wrapper, API.get_new_directory(item, directory), load_opts, prev_dirs, item)
+        end
+    end
+    return true
+end
+
+--a wrapper function that ensures the concurrent_loadlist_parse is run correctly
+function concurrent_loadlist_wrapper(directory, opts, prev_dirs, item)
+    --ensures that only a set number of concurrent parses are operating at any one time.
+    --the mpv event queue is seemingly limited to 1000 items, but only async mpv actions like
+    --command_native_async should use that, events like mp.add_timeout (which coroutine.sleep() uses) should
+    --be handled enturely on the Lua side with a table, which has a significantly larger maximum size.
+    while (opts.concurrency > o.max_concurrency) do
+        API.coroutine.sleep(0.1)
+    end
+    opts.concurrency = opts.concurrency + 1
+
+    local success = concurrent_loadlist_parse(directory, opts, prev_dirs, item)
+    opts.concurrency = opts.concurrency - 1
+    if not success then item._sublist = {} end
+    if coroutine.status(opts.co) == "suspended" then API.coroutine.resume_err(opts.co) end
+end
+
+--recursively appends items to the playlist, acts as a consumer to the previous functions producer;
+--if the next directory has not been parsed this function will yield until the parse has completed
+local function concurrent_loadlist_append(list, load_opts)
+    local directory = list._directory
+
+    for _, item in ipairs(list) do
+        if not sub_extensions[ API.get_extension(item.name, "") ]
+        and not audio_extensions[ API.get_extension(item.name, "") ]
+        then
+            while (not item._sublist and API.parseable_item(item)) do
+                coroutine.yield()
+            end
+
+            if API.parseable_item(item) then
+                concurrent_loadlist_append(item._sublist, load_opts)
+            else
+                loadfile(API.get_full_path(item, directory), load_opts)
+            end
+        end
+    end
+end
+
 --recursive function to load directories using the script custom parsers
 --returns true if any items were appended to the playlist
-local function custom_loadlist_recursive(directory, flag, prev_dirs)
+local function custom_loadlist_recursive(directory, load_opts, prev_dirs)
     --prevents infinite recursion from the item.path or opts.directory fields
     if prev_dirs[directory] then return end
     prev_dirs[directory] = true
@@ -1166,53 +1272,58 @@ local function custom_loadlist_recursive(directory, flag, prev_dirs)
     --if we can't parse the directory then append it and hope mpv fares better
     if list == nil then
         msg.warn("Could not parse", directory, "appending to playlist anyway")
-        mp.commandv("loadfile", directory, flag)
-        flag = "append-play"
+        loadfile(directory, load_opts.flag)
         return true
     end
 
     directory = opts.directory or directory
     if directory == "" then return end
 
-    --this item appending logic is very sensitive as it impacts how multiselected items are added to the playlist
-    local item_appended = false
     for _, item in ipairs(list) do
         if not sub_extensions[ API.get_extension(item.name, "") ]
         and not audio_extensions[ API.get_extension(item.name, "") ]
         then
-            if item.type == "dir" or parseable_extensions[API.get_extension(item.name, "")] then
-                if custom_loadlist_recursive( API.get_new_directory(item, directory) , flag, prev_dirs) then
-                    flag = "append-play"
-                    item_appended = true
-                end
+            if API.parseable_item(item) then
+                custom_loadlist_recursive( API.get_new_directory(item, directory) , load_opts, prev_dirs)
             else
                 local path = API.get_full_path(item, directory)
-
-                msg.verbose("Appending", path, "to the playlist")
-                mp.commandv("loadfile", path, flag)
-                flag = "append-play"
-                item_appended = true
+                loadfile(path, load_opts)
             end
         end
     end
-    return item_appended
 end
 
 
---a wrapper for the custom_loadlist_recursive function to handle the flags
-local function loadlist(directory, flag)
-    local item_appended = custom_loadlist_recursive(directory, flag, {})
-    if not item_appended then msg.warn(directory, "contained no valid files") end
-    return item_appended
+--a wrapper for the custom_loadlist_recursive function
+local function loadlist(item, opts)
+    local dir = API.get_full_path(item, opts.directory)
+    local num_items = opts.items_appended
+
+    if o.concurrent_recursion then
+        item = API.copy_table(item)
+        opts.co = API.coroutine.assert()
+        opts.concurrency = 0
+
+        --we need the current coroutine to suspend before we run the first parse operation, so
+        --we schedule the coroutine to run on the mpv event queue
+        mp.add_timeout(0, function()
+            API.coroutine.run(concurrent_loadlist_wrapper, dir, opts, {}, item)
+        end)
+        concurrent_loadlist_append({item, _directory = opts.directory}, opts)
+    else
+        custom_loadlist_recursive(dir, opts, {})
+    end
+
+    if opts.items_appended == num_items then msg.warn(dir, "contained no valid files") end
 end
 
 --load playlist entries before and after the currently playing file
-local function autoload_dir(path)
+local function autoload_dir(path, opts)
     if o.autoload_save_current and path == current_file.path then
         mp.commandv("write-watch-later-config") end
 
     --loads the currently selected file, clearing the playlist in the process
-    mp.commandv("loadfile", path)
+    loadfile(path, opts)
 
     local pos = 1
     local file_count = 0
@@ -1224,7 +1335,7 @@ local function autoload_dir(path)
             local p = API.get_full_path(item)
 
             if p == path then pos = file_count
-            else mp.commandv("loadfile", p, "append") end
+            else loadfile( p, opts) end
 
             file_count = file_count + 1
         end
@@ -1233,29 +1344,28 @@ local function autoload_dir(path)
 end
 
 --runs the loadfile or loadlist command
-local function loadfile(item, flag, autoload, directory)
-    local path = API.get_full_path(item, directory)
-    if item.type == "dir" or parseable_extensions[ API.get_extension(item.name, "") ] then
-        return loadlist(path, flag) end
+local function open_item(item, opts)
+    if API.parseable_item(item) then
+        return loadlist(item, opts)
+    end
 
+    local path = API.get_full_path(item, opts.directory)
     if sub_extensions[ API.get_extension(item.name, "") ] then
-        mp.commandv("sub-add", path, flag == "replace" and "select" or "auto")
+        mp.commandv("sub-add", path, opts.flag == "replace" and "select" or "auto")
     elseif audio_extensions[ API.get_extension(item.name, "") ] then
-        mp.commandv("audio-add", path, flag == "replace" and "select" or "auto")
+        mp.commandv("audio-add", path, opts.flag == "replace" and "select" or "auto")
     else
-        if autoload then autoload_dir(path)
-        else mp.commandv('loadfile', path, flag) end
-        return true
+        if opts.autoload then autoload_dir(path, opts)
+        else loadfile(path, opts) end
     end
 end
 
 --handles the open options as a coroutine
 --once loadfile has been run we can no-longer guarantee synchronous execution - the state values may change
 --therefore, we must ensure that any state values that could be used after a loadfile call are saved beforehand
-local function open_file_coroutine(flag, autoload)
+local function open_file_coroutine(opts)
     if not state.list[state.selected] then return end
-    if flag == 'replace' then close() end
-    local directory = state.directory
+    if opts.flag == 'replace' then close() end
 
     --we want to set the idle option to yes to ensure that if the first item
     --fails to load then the player has a chance to attempt to load further items (for async append operations)
@@ -1274,23 +1384,26 @@ local function open_file_coroutine(flag, autoload)
         --the currently selected file will be loaded according to the flag
         --the flag variable will be switched to append once a file is loaded
         for i=1, #selection do
-            if loadfile(selection[i], flag, autoload, directory) then flag = "append-play" end
+            open_item(selection[i], opts)
         end
 
-    elseif flag == 'replace' then
-        local item = state.list[state.selected]
-        down_dir()
-        loadfile(item, flag, autoload ~= o.autoload, directory)
     else
-        loadfile(state.list[state.selected], flag, false, directory)
+        local item = state.list[state.selected]
+        if opts.flag == "replace" then down_dir() end
+        open_item(item, opts)
     end
 
     if mp.get_property("idle") == "yes" then mp.set_property("idle", idle) end
 end
 
 --opens the selelected file(s)
-local function open_file(flag, autoload_dir)
-    API.coroutine.run(open_file_coroutine, flag, autoload_dir)
+local function open_file(flag, autoload)
+    API.coroutine.run(open_file_coroutine, {
+        flag = flag,
+        autoload = (autoload ~= o.autoload and flag == "replace"),
+        directory = state.directory,
+        items_appended = 0
+    })
 end
 
 
