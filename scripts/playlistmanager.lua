5e7766fa65c5d41b965a1f519bdc1528fb429833
dyphire <qimoge@gmail.com>
2022-07-03 17:24:30 +0800

    playlistmanager.lua 同步上游更新
    Former-commit-id: 8ecccf227715b13a23051e72c87ffa81866b8c57 [formerly a53e5ad82737191170f281c0949c27818b2628de]
    Former-commit-id: 744176e93a94c26e35509c06eed22fc275e0695c
    Former-commit-id: 79c557e0a81e8f2d48a9a094f738be1b213895e9

--- scripts/playlistmanager.lua
+++ scripts/playlistmanager.lua
@@ -64,8 +64,10 @@ local settings = {
     ]
   ]],
 
-  --loadfiles at startup if there is 0 or 1 items in playlist, if 0 uses worḱing dir for files
+  --loadfiles at startup if 1 or more items in playlist
   loadfiles_on_start = false,
+  -- loadfiles from working directory on idle startup
+  loadfiles_on_idle_start = false,
 
   --sort playlist on mpv start
   sortplaylist_on_start = false,
@@ -165,6 +167,9 @@ local settings = {
   -- what to show when playlist is truncated
   playlist_sliced_prefix = "...",
   playlist_sliced_suffix = "...",
+
+  --output visual feedback to OSD for tasks
+  display_osd_feedback = false,
 
   -- reset cursor navigation when playlist is not visible
   reset_cursor_on_close = true
@@ -801,7 +806,9 @@ function save_playlist()
       file:write(fullpath, "\n")
       i=i+1
     end
-    msg.info("Playlist written to: "..savepath)
+    local saved_msg = "Playlist written to: "..savepath
+    if settings.display_osd_feedback then mp.osd_message(saved_msg) end
+    msg.info(saved_msg)
     file:close()
   end
 end
@@ -860,7 +867,11 @@ function reverseplaylist()
   for outer=1, length-1, 1 do
     mp.commandv('playlist-move', outer, 0)
   end
-  if playlist_visible then showplaylist() end
+  if playlist_visible then
+    showplaylist()
+  elseif settings.display_osd_feedback then
+    mp.osd_message("Playlist reversed")
+  end
 end
 
 function shuffleplaylist()
@@ -871,7 +882,11 @@ function shuffleplaylist()
   mp.commandv("playlist-move", pos, math.random(0, plen-1))
   mp.set_property('playlist-pos', 0)
   refresh_globals()
-  if playlist_visible then showplaylist() end
+  if playlist_visible then
+    showplaylist()
+  elseif settings.display_osd_feedback then
+    mp.osd_message("Playlist shuffled")
+  end
 end
 
 function bind_keys(keys, name, func, opts)
@@ -945,7 +960,7 @@ if not settings.dynamic_binds then
   add_keybinds()
 end
 
-if settings.loadfiles_on_start and mp.get_property_number('playlist-count', 0) == 0 then
+if settings.loadfiles_on_idle_start and mp.get_property_number('playlist-count', 0) == 0 then
   playlist()
 end
 
