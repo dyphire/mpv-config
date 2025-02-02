51a7c9d3464fd46d3d8a9831d138ef3e5f14da50
dyphire <qimoge@gmail.com>
2025-02-02 11:03:02 +0800

    update

--- scripts/sub-assrt.lua
+++ scripts/sub-assrt.lua
@@ -178,31 +178,38 @@ local function clean_name(name)
            :gsub("[!@#%.%?%+%-%%&*_=,/~`]+$", "")
 end
 
+-- Formatters for media titles
 local formatters = {
     {
-        regex = "^(.-)%s*[_%.%s]%s*(%d%d%d%d[_%.%s]%d%d[_%.%s]%d%d)%s*[_%.%s]?(.-)%s*[_%.%s]%d+[pPkKxXbBfF]",
-        format = function(name, date, subtitle)
+        regex = "^(.-)%s*[_%.%s]%s*(%d%d%d%d)[_%.%s]%d%d[_%.%s]%d%d%s*[_%.%s]?(.-)%s*[_%.%s]%d+[pPkKxXbBfF]",
+        format = function(name, year, subtitle)
             local title = clean_name(name)
             if subtitle then
                 title = title .. ": " .. subtitle:gsub("%.", " "):gsub("^%s*(.-)%s*$", "%1")
             end
-            return title .. " (" .. date .. ")"
+            return title .. " (" .. year .. ")"
         end
     },
     {
-        regex = "^(.-)%s*[_%.%s]%s*(%d%d%d%d)%s*[_%.%s]%s*[sS](%d+)[%.%-%s:]?[eE](%d+)",
+        regex = "^(.-)%s*[_%.%s]%s*(%d%d%d%d)%s*[_%.%s]%s*[sS](%d+)[%.%-%s:]?[eE](%d+%.?%d*)",
         format = function(name, year, season, episode)
             return clean_name(name) .. " (" .. year .. ") S" .. season .. "E" .. episode
         end
     },
     {
-        regex = "^(.-)%s*[_%.%s]%s*(%d%d%d%d)%s*[_%.%s]%s*[eEpP]+(%d+)",
+        regex = "^(.-)%s*[_%.%s]%s*(%d%d%d%d)%s*[_%.%s]%s*[eEpP]+(%d+%.?%d*)",
         format = function(name, year, episode)
-            return clean_name(name) .. " (" .. year .. ") [" .. episode .. "]"
+            return clean_name(name) .. " (" .. year .. ") E" .. episode
         end
     },
     {
-        regex = "^(.-)%s*[_%-%.%s]%s*[sS](%d+)[%.%-%s:]?[eE](%d+)",
+        regex = "^(.-)%s*[_%-%.%s]%s*[sS](%d+)[%.%-%s:]?[eE](%d+[%.v]?%d*)%s*[_%.%s]%s*(%d%d%d%d)[^%dhHxXvVpPkKxXbBfF]",
+        format = function(name, season, episode, year)
+            return clean_name(name) .. " (" .. year .. ") S" .. season .. "E" .. episode:gsub("v%d+$","")
+        end
+    },
+    {
+        regex = "^(.-)%s*[_%-%.%s]%s*[sS](%d+)[%.%-%s:]?[eE](%d+%.?%d*)",
         format = function(name, season, episode)
             return clean_name(name) .. " S" .. season .. "E" .. episode
         end
@@ -210,55 +217,55 @@ local formatters = {
     {
         regex = "^(.-)%s*[_%.%s]%s*(%d+)[nrdsth]+[_%.%s]%s*[sS]eason[_%.%s]%s*%[(%d+[%.v]?%d*)%]",
         format = function(name, season, episode)
-            return clean_name(name) .. " S" .. season .. "E" .. episode
+            return clean_name(name) .. " S" .. season .. "E" .. episode:gsub("v%d+$","")
         end
     },
     {
-        regex = "^(.-)%s*[eEpP]+(%d+)[_%.%s]%s*(%d%d%d%d)[^%dhHxXvVpPkKxXbBfF]",
+        regex = "^(.-)%s*[^dD][eEpP]+(%d+[%.v]?%d*)[_%.%s]%s*(%d%d%d%d)[^%dhHxXvVpPkKxXbBfF]",
         format = function(name, episode, year)
-            return clean_name(name) .. " (" .. year .. ") [" .. episode .. "]"
+            return clean_name(name) .. " (" .. year .. ") E" .. episode:gsub("v%d+$","")
         end
     },
     {
-        regex = "^(.-)%s*[eEpP]+(%d+)",
+        regex = "^(.-)%s*[^dD][eEpP]+(%d+%.?%d*)",
         format = function(name, episode)
-            return clean_name(name) .. " [" .. episode .. "]"
+            return clean_name(name) .. " E" .. episode
+        end
+    },
+    {
+        regex = "^(.-)%s*第%s*(%d+[%.v]?%d*)%s*[话回集]",
+        format = function(name, episode)
+            return clean_name(name) .. " E" .. episode:gsub("v%d+$","")
         end
     },
     {
         regex = "^(.-)%s*%[(%d+[%.v]?%d*)%]",
         format = function(name, episode)
-            return clean_name(name) .. " [" .. episode .. "]"
+            return clean_name(name) .. " E" .. episode:gsub("v%d+$","")
         end
     },
     {
-        regex = "^(.-)%s*%[(%d+[%.v]?%d*%(%a+%))%]",
+        regex = "^(.-)%s*%[(%d+[%.v]?%d*)%(%a+%)%]",
         format = function(name, episode)
-            return clean_name(name) .. " [" .. episode .. "]"
+            return clean_name(name) .. " E" .. episode:gsub("v%d+$","")
         end
     },
     {
-        regex = "^(.-)%s*[%-#]%s*(%d+)%s*",
+        regex = "^(.-)%s*[%-#]%s*(%d+%.?%d*)%s*",
         format = function(name, episode)
-            return clean_name(name) .. " [" .. episode .. "]"
-        end
-    },
-    {
-        regex = "^(.-)%s*%[(%d+)%]%D+",
-        format = function(name, episode)
-            return clean_name(name) .. " [" .. episode .. "]"
+            return clean_name(name) .. " E" .. episode
         end
     },
     {
         regex = "^(.-)%s*[%[%(]([OVADSPs]+)[%]%)]",
-        format = function(name, SP)
-            return clean_name(name) .. " [" .. SP .. "]"
+        format = function(name, sp)
+            return clean_name(name) .. " [" .. sp .. "]"
         end
     },
     {
-        regex = "^%((%d%d%d%d%.?%d?%d?%.?%d?%d?)%)%s*(.-)%s*[%(%[]",
-        format = function(date, name)
-            return clean_name(name) .. " (" .. date .. ")"
+        regex = "^%((%d%d%d%d)%.?%d?%d?%.?%d?%d?%)%s*(.-)%s*[%(%[]",
+        format = function(year, name)
+            return clean_name(name) .. " (" .. year .. ")"
         end
     },
     {
@@ -279,7 +286,7 @@ local function format_filename(title)
     for _, formatter in ipairs(formatters) do
         local matches = {title:match(formatter.regex)}
         if #matches > 0 then
-            title = formatter.format(table.unpack(matches))
+            title = formatter.format(unpack(matches))
             return title
         end
     end
@@ -589,7 +596,7 @@ function update_menu_uosc(type, title, text, footnote, cmd, query)
         type = type,
         title = title,
         search_style = cmd and "palette" or "on_demand",
-        search_debounce = "submit",
+        search_debounce = cmd and "submit" or 0,
         on_search = cmd,
         footnote = footnote,
         search_suggestion = query,
